944695b957ee10e3365c05579af6167a14759a49
luiz felipe picoli <felipedelibra@hotmail.com>
2021-11-24 16:13:53 -0300

    projeto inicial

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+
+    }),
+    TypeOrmModule.forRoot(),
+    UsersModule
+  ],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule {}
