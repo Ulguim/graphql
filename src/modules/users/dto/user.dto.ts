0985441075998d30c13ce20d99c4b4ea0b238045
luizpicoli <30914683+luizpicoli@users.noreply.github.com>
2021-12-06 09:10:40 -0300

    Create user.dto.ts

---
+++ src/modules/users/dto/user.dto.ts
@@ -0,0 +1,23 @@
+import { FilterableField, FilterableOffsetConnection, FilterableRelation } from '@nestjs-query/query-graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
+//import { ScheduleDTO } from 'src/modules/schedules/dto/schedule.dto';
+import { StaffDTO } from 'src/modules/staffs/dto/staff.dto';
+import { ScheduleDTO } from 'src/modules/schedules/dto/schedule.dto';
+//import { staffsDTO } from 'src/modules/staffs/dto/staffs.dto';
+
+//import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@ObjectType('User')
+//@FilterableOffsetConnection('Schedule',() => ScheduleDTO,{ nullable: true })
+export class UserDTO extends StaffDTO { 
+
+  @FilterableField()
+  name: string;
+  
+  @FilterableField()
+  created:Date;
+
+  @FilterableField({nullable: true})
+  cpf:number;
+
+}
