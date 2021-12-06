95af813a2fece7d805974c4a69d5739b329a5537
luizpicoli <30914683+luizpicoli@users.noreply.github.com>
2021-12-06 09:11:48 -0300

    Update users.module.ts

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,8 +1,29 @@
+import { NestjsQueryGraphQLModule, PagingStrategies } from '@nestjs-query/query-graphql';
+import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersResolver } from './users.resolver';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import { UserDTO } from './dto/user.dto';
+import { User } from './entities/user.entity';
+import { UserService } from './users.service';
 
 @Module({
-  providers: [UsersResolver, UsersService]
+  imports:[
+    NestjsQueryGraphQLModule.forFeature({
+           services : [UserService],
+      imports: [NestjsQueryTypeOrmModule.forFeature([User])],
+      
+      resolvers: [{
+         DTOClass: UserDTO, 
+         EntityClass: User,
+         CreateDTOClass:CreateUserInput,
+         UpdateDTOClass:UpdateUserInput,
+         ServiceClass: UserService,
+         enableTotalCount:true,
+         pagingStrategy: PagingStrategies.OFFSET,
+        }],
+    }),
+  ],
+  providers: []
 })
 export class UsersModule {}
